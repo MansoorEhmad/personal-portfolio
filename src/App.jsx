cc8b09cae7fbd2f627abaf3162c083ca84b2670a
Khalid Mansoor <97522481+MansoorEhmad@users.noreply.github.com>
2024-03-23 01:22:39 +0500

    common chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import Header from "./components/Header";
 import PopupMobileMenu from "./components/PopupMobileMenu";
-import Main from "./components/Main";
+import Main from "./components/MainContent";
 import Footer from "./components/Footer";
-import "./js/vendor/jquery.js";
-import "./js/vendor/modernizer.min.js";
-import "./js/vendor/feather.min.js";
-import "./js/vendor/slick.min.js";
-import "./js/vendor/bootstrap.js";
-import "./js/vendor/text-type.js";
-import "./js/vendor/wow.js";
-import "./js/vendor/aos.js";
-import "./js/vendor/particles.js";
-import "./js/main.js";
+import "../public/js/main.js";
+
 
 function App() {
   return (
